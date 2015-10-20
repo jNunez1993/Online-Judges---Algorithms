7f6f55a2b8a599612c65b59dc0165a8efa8caee0
Juan Nunez <njuan1993@yahoo.com>
2015-10-20 00:54:09 -0400

    solutions

--- UVA/UVA10911.cpp
+++ UVA/UVA10911.cpp
@@ -1,56 +1,54 @@
 #include <bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define INF 2e9
+#define INF 1e9
 using namespace std;
 #define DEBUG(x)cout<<x<<endl;
-int n;
-int x[1000];
-int y[1000];
-//check these values later
+
+
+int x[10];
+int y[10];
+int N;
 double memo[1<<16];
 
-double dist(int c1,int c2){
-	return sqrt(pow(x[c1]-x[c2],2)+pow(y[c1]-y[c2],2));
+
+double dist(int i,int j){
+	return sqrt(pow(x[i]-x[j],2)+pow(y[i]-y[j],2));
 }
 
 double solve(int bitmask){
-	if(bitmask==(1<<n)-1)return 0;
+	if(bitmask>=(pow(2,2*N)-1)){
+		return 0;
+	}
 	if(memo[bitmask]!=0)return memo[bitmask];
-	double ans=INF;
-	int c1=0;
-	int c2=0;
-	while(c1<n && ((1 & (bitmask>>c1))!=0))c1++;
-
-	for(c2=c1+1;c2<n;c2++){
-		if((1 & (bitmask>>c2))==0){
-			ans=std::min(ans,dist(c1,c2)+solve(bitmask | 1<<c1 | 1<<c2));
+	double minDist=INF;
+	int i=0;
+	int j=0;
+	for(i=0;i<2*N;i++){
+		if((bitmask & (1<<i))==0)break;
+	}
+	for(j=i+1;j<2*N;j++){
+		if((bitmask & (1<<j))==0){
+			minDist=std::min(minDist,solve(bitmask | (1<<i) | (1<<j))+ dist(i,j));
 		}
 	}
-	return memo[bitmask]=ans;
+
+	return memo[bitmask]=minDist;
 
 }
 
-
-
 int main(){
-	//assert(freopen("f.in","r",stdin));
-	//assert(freopen("f.out","w",stdout));
-	string name;
-	int x1,y1;
-	int T=1;
-	while(cin>>n){
-		if(n==0)break;
+	int CASE=1;
+	while(cin>>N){
+		if(N==0)break;
 		memset(memo,0,sizeof memo);
-		n*=2;
-		for(int i=0;i<n;i++){
-			cin>>name>>x1>>y1;
-			x[i]=x1;
-			y[i]=y1;
+		memset(x,0,sizeof x);
+		memset(y,0,sizeof y);
+		string s;
+		int xc,yc;
+		for(int i=0;i<2*N;i++){
+			cin>>s>>x[i]>>y[i];
 		}
-		
-		double ans=solve(0);
-		printf("Case %d: %.2f\n",T++,ans);
-		
+		double sol= solve(0);
+		printf("Case %d: %.2f\n",CASE++,sol);
 	}
-
 }
